2da56ceaa9e41bf77b31eeb7a859ec37fbd06e35
abduallahbarmu <abduallah.imu2@gmail.com>
2023-10-25 15:47:14 +0300

    create home page , create navbar & footer components , and initlize timeline & events componets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EventsComponent } from './events/events.component';
+import { HeroComponent } from './home/hero/hero.component';
+import { TimelineComponent } from './home/timeline/timeline.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    EventsComponent,
+    HeroComponent,
+    TimelineComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+ 
+ export const environment = {
+    production:true,
+    apiBaseUrl:'https://production-api.example.com',
+}
